2123c67830b21a28c1d7e61777adcb7c37beadbb
T.Miura <mtaiga0213@icloud.com>
2025-05-18 00:20:58 +0900

    feat: OAuth後ログイン画面の表示

--- practices/supabase-p/src/App.tsx
+++ practices/supabase-p/src/App.tsx
@@ -1,39 +1,50 @@
 import { useEffect, useState } from "react";
+import Auth from "./Auth";
+import useInstrument from "./useInstrument";
+import type { User } from "@supabase/supabase-js";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-);
-
-interface Instrument {
-  name: string;
-}
-
 function App() {
-  const [instruments, setInstruments] = useState<Instrument[]>([]);
-
+  const [user, setUser] = useState<User | null>(null);
+  
   useEffect(() => {
-    getInstruments();
-  }, []);
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      setUser(user)
+    })
+  }, [])
 
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+  );
+  
+  const { instruments, error, isLoading } = useInstrument();
   console.log(instruments);
-  async function getInstruments() {
-    const { data } = await supabase.from("instruments").select();
-    if (data) {
-      setInstruments(data);
+
+  async function signOut() {
+    const { error } = await supabase.auth.signOut()
+    if (error) {
+      console.error("Error signing out:", error.message);
     } else {
-      setInstruments([]);
+      console.log("Signed out successfully");
     }
   }
+  if (error) return <p>error</p>
+  if (isLoading) return <p>loading</p>
 
   return (
     <>
       <ul>
-        {instruments.map((instrument) => (
-          <li key={instrument.name}>{instrument.name}</li>
+        {instruments?.map((instrument) => (
+          <li key={instrument.id}>{instrument.id} : {instrument.name}</li>
         ))}
       </ul>
+      {user ? (
+        <div>
+          <p>Logged In</p>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
+      ): <Auth />}
     </>
   );
 }
