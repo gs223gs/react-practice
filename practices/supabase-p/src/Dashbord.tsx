30d1939621d522d34c5288a4178d13c2fb3bbbdc
T.Miura <mtaiga0213@icloud.com>
2025-05-18 15:38:31 +0900

    feat: form を実装

--- practices/supabase-p/src/Dashbord.tsx
+++ practices/supabase-p/src/Dashbord.tsx
@@ -1,13 +1,39 @@
 import "./App.css";
+import { useState } from "react";
 
 import Header from "./Header";
+import useCRUD from "./useCRUD";
+import useSession from "./useSession";
 
 const Dashbord = () => {
-  // const { userdata } = useSession();
+  const { userdata } = useSession();
+  const { createTodo } = useCRUD();
+  const [task, setTask] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTask("");
+
+    if (userdata?.id) {
+      createTodo(task, userdata.id);
+    }
+  };
 
   return (
     <>
       <Header />
+      <form onSubmit={handleSubmit}>
+        <span>タスク入力</span>
+        <input
+          type="text"
+          onChange={(e) => setTask(e.target.value)}
+          value={task}
+          className="border-2 border-gray-300 rounded-md p-2"
+        />
+        <button type="submit" className="bg-blue-500 text-white rounded-md p-2">
+          追加
+        </button>
+      </form>
     </>
   );
 };
