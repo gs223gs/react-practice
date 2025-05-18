6a9dea7aa2ad5f1dde557805ed509b0dfb7d5096
T.Miura <mtaiga0213@icloud.com>
2025-05-18 14:27:41 +0900

    feat: ユーザー情報，ログイン情報をヘッダーとした
    jotaiでuserdataを管理するべきである

--- practices/supabase-p/src/Dashbord.tsx
+++ practices/supabase-p/src/Dashbord.tsx
@@ -1,32 +1,14 @@
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import useSession from "./useSession";
-import supabase from "./supabase";
+import "./App.css";
+
+import Header from "./Header";
 
 const Dashbord = () => {
-  const { userdata} = useSession();
+  // const { userdata } = useSession();
 
-  const signOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (!error) {
-      window.location.href = "/";
-    }
-    return error;
-  };
   return (
-        <div>
-          <p>Logged In</p>
-          <p>{userdata?.email}</p>
-          <p>{userdata?.user_metadata.name}</p>
-          <Avatar className="w-32 h-32 mx-auto">
-            <AvatarImage src={userdata?.user_metadata.avatar_url} />
-            <AvatarFallback>
-              <p>T.Miura</p>
-            </AvatarFallback>
-          </Avatar>
-
-          <Button onClick={signOut}>Sign Out</Button>
-        </div>
+    <>
+      <Header />
+    </>
   );
 };
 
---
+++ practices/supabase-p/src/Header.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import useSession from "./useSession";
+
+import useOAuth from "./useOAuth";
+const Header = () => {
+  const { userdata } = useSession();
+  const { signOut } = useOAuth();
+  return (
+    <div className="flex justify-between items-center m-2 p-2 rounded-4xl">
+      <p className="text-2xl font-bold">Logged In</p>
+      <div className="flex justify-end items-center">
+        <p className="font-bold">{userdata?.email}</p>
+        <p className="font-bold">{userdata?.user_metadata.name}</p>
+        <div className="flex gap-2">
+          <Avatar>
+            <AvatarImage src={userdata?.user_metadata.avatar_url} />
+            <AvatarFallback>
+              <p>T.Miura</p>
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <Button onClick={signOut} className="">
+          Sign Out
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
