8c274098cee77ab72db990279f0dc7a6532499a8
T.Miura <mtaiga0213@icloud.com>
2025-05-18 22:20:23 +0900

    ui: todoがない時の表示を変更 divを分けた

--- practices/supabase-p/src/components/TodoList.tsx
+++ practices/supabase-p/src/components/TodoList.tsx
@@ -1,27 +1,33 @@
-import '../App.css'
+import "../App.css";
 import useSession from "../hooks/useSession";
 import { useTodos } from "../hooks/useTodos";
 import TodoItem from "./TodoItem";
 
 const TodosList = () => {
   const { userdata } = useSession();
-  const { todos, isLoading, isError, mutate} = useTodos(userdata?.id ?? "");
-  if(!userdata?.id){
-    return <div>ログインしてください</div>
+  const { todos, isLoading, isError, mutate } = useTodos(userdata?.id ?? "");
+  if (!userdata?.id) {
+    return <div>ログインしてください</div>;
   }
-  if(isLoading) return <div>Loading...</div>
-  if(isError) return <div>Error: {isError.message}</div>
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {isError.message}</div>;
 
-  mutate()
-  console.log("TodoList render")
+  mutate();
+  console.log("TodoList render");
   return (
-    <div className="grid grid-cols-3 gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
-      {todos?.length == 0 && <div className="p-4 bg-red-100 rounded-lg shadow-md ">Todoがありません</div>}
-      {todos &&
-        todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} mutate={mutate} />
-        ))}
-    </div>
+    <>
+      {todos?.length == 0 && (
+        <div className="p-4 bg-red-100 rounded-lg shadow-md ">
+          Todoがありません
+        </div>
+      )}
+      <div className="grid grid-cols-3 gap-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+        {todos &&
+          todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} mutate={mutate} />
+          ))}
+      </div>
+    </>
   );
 };
 
