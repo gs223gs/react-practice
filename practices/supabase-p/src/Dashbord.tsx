07f32ac4e2f6f96ca3d12ec0de07b793c0e0f76a
T.Miura <mtaiga0213@icloud.com>
2025-05-18 16:45:37 +0900

    feat: Todoリストの取得を実装　Effectでいいのか...?

--- practices/supabase-p/src/Dashbord.tsx
+++ practices/supabase-p/src/Dashbord.tsx
@@ -1,14 +1,23 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Header from "./Header";
 import useCRUD from "./useCRUD";
 import useSession from "./useSession";
+import type { Todos } from "./todos.type";
 
 const Dashbord = () => {
+  const [todos, setTodos] = useState<Todos | null>(null);
   const { userdata } = useSession();
-  const { createTodo } = useCRUD();
+  const { createTodo, fetchTodos } = useCRUD();
   const [task, setTask] = useState<string>("");
+
+  useEffect(() => {
+    if (userdata?.id) {
+      fetchTodos(userdata.id).then((todos) => setTodos(todos));
+      console.log("fetch");
+    }
+  }, [userdata?.id]);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -34,6 +43,16 @@ const Dashbord = () => {
           追加
         </button>
       </form>
+      <div>
+        <div>todos</div>
+        {/* {todos == null && <div>Todoがありません</div>} */}
+        {todos &&
+          todos.map((todo) => (
+            <div key={todo.id}>
+              {todo.title} {todo.is_done}
+            </div>
+          ))}
+      </div>
     </>
   );
 };
