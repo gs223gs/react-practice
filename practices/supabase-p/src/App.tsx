b33d91a7b8109b00c00c1067b5d930fbf4f9ae12
T.Miura <mtaiga0213@icloud.com>
2025-05-18 13:38:20 +0900

    refactor: コンポーネントの粒度を細かく

--- practices/supabase-p/src/App.tsx
+++ practices/supabase-p/src/App.tsx
@@ -1,47 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import Auth from "./Auth";
-import useInstrument from "./useInstrument";
-import useOAuth from "./useOAuth";
 
 import "./App.css";
+import Dashbord from "./Dashbord";
+import useSession from "./useSession";
 
 function App() {
-  const { userdata, signOut } = useOAuth();
-
-  const { instruments, error, isLoading } = useInstrument();
-
-  if (error) return <p>error</p>;
-  if (isLoading) return <p>loading</p>;
-
+  const { userdata } = useSession();
+  console.log(userdata);
   return (
     <>
-      {userdata && (
-        <>
-          <div>
-            <p>Logged In</p>
-            <p>{userdata.email}</p>
-            <p>{userdata.user_metadata.name}</p>
-            <Avatar className="w-32 h-32 mx-auto">
-              <AvatarImage src={userdata.user_metadata.avatar_url} />
-              <AvatarFallback>
-              <p>T.Miura</p>
-              </AvatarFallback>
-            </Avatar>
-
-            <Button onClick={signOut}>Sign Out</Button>
-          </div>
-          <div>
-            <ul>
-              {instruments?.map((instrument) => (
-                <li key={instrument.id}>
-                  {instrument.id} : {instrument.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </>
-      )}
+      {userdata && <Dashbord />}
       {!userdata && <Auth />}
     </>
   );
