2ed7edcd21fb92755d035c7de4c571891b740fa1
T.Miura <mtaiga0213@icloud.com>
2025-05-18 18:23:03 +0900

    refactor: コンポーネントの粒度を細かく変更

--- practices/supabase-p/src/Dashbord.tsx
+++ practices/supabase-p/src/Dashbord.tsx
@@ -1,24 +1,19 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import Header from "./Header";
 import useCRUD from "./useCRUD";
 import useSession from "./useSession";
-import type { Todos } from "./todos.type";
-
+import TodoList from "./TodoList";
 const Dashbord = () => {
-  const [todos, setTodos] = useState<Todos | null>(null);
+
   const { userdata } = useSession();
-  const { createTodo, fetchTodos } = useCRUD();
+  const { createTodo} = useCRUD();
   const [task, setTask] = useState<string>("");
 
-  useEffect(() => {
-    if (userdata?.id) {
-      fetchTodos(userdata.id).then((todos) => setTodos(todos));
-      console.log("fetch");
-    }
-  }, [userdata?.id]);
 
+
+  console.log("render")
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setTask("");
@@ -43,16 +38,8 @@ const Dashbord = () => {
           追加
         </button>
       </form>
-      <div>
-        <div>todos</div>
-        {/* {todos == null && <div>Todoがありません</div>} */}
-        {todos &&
-          todos.map((todo) => (
-            <div key={todo.id}>
-              {todo.title} {todo.is_done}
-            </div>
-          ))}
-      </div>
+
+      <TodoList />
     </>
   );
 };
