abf9dfd9da657cb01e8cd89e30e70f29451fc3d8
T.Miura <mtaiga0213@icloud.com>
2025-05-18 14:04:34 +0900

    feat:crudの仮実装

---
+++ practices/supabase-p/src/useCRUD.tsx
@@ -0,0 +1,37 @@
+import supabase from "./supabase";
+
+const useCRUD = () => {
+  const createTodo = async (title: string, userId: string) => {
+    const { data, error } = await supabase
+      .from("todos")
+      .insert([{ title, user_id: userId }]);
+    if (error) console.error("Error creating todo:", error);
+    return data;
+  };
+  
+  const fetchTodos = async (userId: string) => {
+    const { data, error } = await supabase
+      .from("todos")
+      .select("*")
+      .eq("user_id", userId);
+    if (error) console.error("Error fetching todos:", error);
+    return data;
+  };
+
+  const updateTodo = async (id: string, isDone: boolean) => {
+    const { data, error } = await supabase
+      .from("todos")
+      .update({ is_done: isDone })
+      .eq("id", id);
+    if (error) console.error("Error updating todo:", error);
+    return data;
+  };
+
+  const deleteTodo = async (id: string) => {
+    const { data, error } = await supabase.from("todos").delete().eq("id", id);
+    if (error) console.error("Error deleting todo:", error);
+    return data;
+  };
+  return { createTodo, fetchTodos, updateTodo, deleteTodo };
+};
+export default useCRUD;
