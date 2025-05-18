014eee6a752be226809f689af5a89bd771318938
T.Miura <mtaiga0213@icloud.com>
2025-05-18 22:59:07 +0900

    feat: shadcn で formの実装
    UIが破壊されたが，全ての目標を達成したため終了

--- practices/supabase-p/src/components/Dashbord.tsx
+++ practices/supabase-p/src/components/Dashbord.tsx
@@ -1,47 +1,63 @@
 import "../App.css";
-import { useState } from "react";
-
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
 import Header from "./Header";
 import useCRUD from "../hooks/useCRUD";
 import useSession from "../hooks/useSession";
 import TodoList from "../components/TodoList";
+import { Input } from "@/components/ui/input";
+import useFormWithValidation from "../hooks/useFormWithValidation";
+import { formSchema } from "../validation/todoFormSchema";
+import { useTodos } from "../hooks/useTodos";
+import { z } from "zod";
 const Dashbord = () => {
+  const form = useFormWithValidation();
   const { userdata } = useSession();
   const { createTodo } = useCRUD();
-  const [task, setTask] = useState<string>("");
+  const { mutate } = useTodos(userdata?.id ?? "");
 
   console.log("render");
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setTask("");
-
+  const handleSubmit = (task: z.infer<typeof formSchema>) => {
     if (userdata?.id) {
-      createTodo(task, userdata.id);
+      createTodo(task.title, userdata.id);
+      mutate();
+      form.reset();
     }
   };
 
   return (
     <>
       <Header />
-      <div className="flex flex-col items-center justify-center">
-        <form onSubmit={handleSubmit}>
-          <span>タスク入力</span>
-          <input
-            type="text"
-            onChange={(e) => setTask(e.target.value)}
-            value={task}
-            className="border-2 border-gray-300 rounded-md p-2"
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white rounded-md p-2"
-          >
-            追加
-          </button>
-        </form>
-
-        <TodoList />
+      <div className="w-full flex flex-col items-center justify-center max-w-md mx-auto gap-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit((data) => handleSubmit(data))}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>タスク入力</FormLabel>
+                  <FormControl>
+                    <Input placeholder="タスクを入力してください" {...field} />
+                  </FormControl>
+                  <FormDescription>タスクを入力してください</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">追加</Button>
+          </form>
+        </Form>
       </div>
+      <TodoList />
     </>
   );
 };
