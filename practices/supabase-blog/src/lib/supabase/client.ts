88edfb1e8358b07fee74df191b8d669b0ec1e3fa
T.Miura <mtaiga0213@icloud.com>
2025-05-19 23:20:05 +0900

    fix: シングルトンに変更

--- practices/supabase-blog/src/lib/supabase/client.ts
+++ practices/supabase-blog/src/lib/supabase/client.ts
@@ -1,8 +1,8 @@
-import { createClient as createSupabaseClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-export function createClient() {
-  return createSupabaseClient(
-    import.meta.env.VITE_SUPABASE_URL!,
-    import.meta.env.VITE_SUPABASE_ANON_KEY!
-  )
-}
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+export default supabase;
