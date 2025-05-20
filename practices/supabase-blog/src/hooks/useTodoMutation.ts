8af20caf6f80b0bb8e578bb85081cea5b9d59e6b
T.Miura <mtaiga0213@icloud.com>
2025-05-20 21:24:10 +0900

    feat: updateメソッドをオブジェクトをそのまま入れるように変更

--- practices/supabase-blog/src/hooks/useTodoMutation.ts
+++ practices/supabase-blog/src/hooks/useTodoMutation.ts
@@ -1,5 +1,6 @@
 import { useSWRConfig } from "swr";
 import supabase from "@/lib/supabase/client";
+import type{ Todo } from "@/types/todos.type";
 
 export function useTodoMutation() {
   const { mutate } = useSWRConfig();
@@ -11,10 +12,10 @@ export function useTodoMutation() {
     mutate("todos");
   };
 
-  const updateTodo = async (id: string, isDone: boolean) => {
+  const updateTodo = async (id: string, todo: Todo) => {
     const { error } = await supabase
       .from("todos")
-      .update({ is_done: isDone })
+      .update({...todo})
       .eq("id", id);
 
     if (error) throw error;
