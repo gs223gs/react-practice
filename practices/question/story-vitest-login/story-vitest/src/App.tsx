f515f39e8f9fa4108fcae0f83ceac8617a3d508d
T.Miura <mtaiga0213@icloud.com>
2025-05-24 13:07:40 +0900

    feat: validation と formの実装

---
+++ practices/question/story-vitest-login/story-vitest/src/App.tsx
@@ -0,0 +1,53 @@
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { ProfileForm } from "@/utils/validations/loginform-zod"
+import './App.css'
+
+function App() {
+  const { form, onSubmit } = ProfileForm()
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    </div>
+
+  )
+}
+
+export default App
