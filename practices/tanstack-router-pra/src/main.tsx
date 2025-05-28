8ba3ec8af4cf796e61f9072cfb9ea054cc89de4c
T.Miura <mtaiga0213@icloud.com>
2025-05-28 21:32:49 +0900

    feat: 定義したrouterを適応

--- practices/tanstack-router-pra/src/main.tsx
+++ practices/tanstack-router-pra/src/main.tsx
@@ -1,26 +1,10 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-
-import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx'
-
-// Import the generated route tree
-import { routeTree } from './routeTree.gen.ts'
-
+import { RouterProvider } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { router } from './routes'
 import './styles.css'
 import reportWebVitals from './reportWebVitals.ts'
-
-// Create a new router instance
-const router = createRouter({
-  routeTree,
-  context: {
-    ...TanStackQueryProvider.getContext(),
-  },
-  defaultPreload: 'intent',
-  scrollRestoration: true,
-  defaultStructuralSharing: true,
-  defaultPreloadStaleTime: 0,
-})
 
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
@@ -29,16 +13,18 @@ declare module '@tanstack/react-router' {
   }
 }
 
+const queryClient = new QueryClient()
+
 // Render the app
-const rootElement = document.getElementById('app')
+const rootElement = document.getElementById('root')
 if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
-    <StrictMode>
-      <TanStackQueryProvider.Provider>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
-      </TanStackQueryProvider.Provider>
-    </StrictMode>,
+      </QueryClientProvider>
+    </React.StrictMode>,
   )
 }
 
