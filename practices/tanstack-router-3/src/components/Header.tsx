40c7480f67b52789e3440297c2304f25c65b1d62
T.Miura <mtaiga0213@icloud.com>
2025-06-01 23:31:29 +0900

    feat: ヘッダーにリンクを追加

--- practices/tanstack-router-3/src/components/Header.tsx
+++ practices/tanstack-router-3/src/components/Header.tsx
@@ -11,6 +11,18 @@ export default function Header() {
         <div className="px-2 font-bold">
           <Link to="/demo/tanstack-query">TanStack Query</Link>
         </div>
+        <div className="px-2 font-bold">
+          <Link to="/test/about">About</Link>
+        </div>
+        <div className="px-2 font-bold">
+          <Link to="/posts">Posts</Link>
+        </div>
+        <div className="px-2 font-bold">
+          <Link to="/posts/tests">Tests</Link>
+        </div>
+        <div className="px-2 font-bold">
+          <Link to="/posts/10">Post 10</Link>
+        </div>
       </nav>
     </header>
   )
